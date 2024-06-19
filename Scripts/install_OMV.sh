ed54ad7a07df5d4a39456a854a0d4bf1a5878b10
Abed <abbe_92@live.com>
2024-06-19 13:09:00 +0300

    fix error handlers

--- Scripts/install_OMV.sh
+++ Scripts/install_OMV.sh
@@ -7,6 +7,15 @@ function error {
   exit 1
 }
 
+# Check log file for specific error message
+function check_log_for_error {
+  if grep -q "Unsupported version.  Only 11 (Bullseye) and 12 (Bookworm) are supported" "$LOG_FILE"; then
+    error "Unsupported version detected in log. OpenMediaVault installation failed."
+  else
+    echo "OpenMediaVault installed successfully."
+  fi
+}
+
 # Check network connectivity
 function check_internet() {
   echo "Checking internet connection..."
@@ -17,10 +26,14 @@ function check_internet() {
   fi
 }
 
+# Install OpenMediaVault
 function install_openmediavault() {
   echo "Installing OpenMediaVault..."
   sudo wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
-  echo "OpenMediaVault installed successfully."
+  
+  # Check if installation failed due to unsupported version
+  check_log_for_error
+  
 }
 
 
--- Scripts/install_OMV_ArgonEon.sh
+++ Scripts/install_OMV_ArgonEon.sh
@@ -7,6 +7,15 @@ function error {
   exit 1
 }
 
+# Check log file for specific error message
+function check_log_for_error {
+  if grep -q "Unsupported version.  Only 11 (Bullseye) and 12 (Bookworm) are supported" "$LOG_FILE"; then
+    error "Unsupported version detected in log. OpenMediaVault installation failed."
+  else
+    echo "OpenMediaVault installed successfully."
+  fi
+}
+
 # Check network connectivity
 function check_internet() {
   echo "Checking internet connection..."
@@ -17,16 +26,22 @@ function check_internet() {
   fi
 }
 
+# Install ArgonEon Display Features
 function install_ArgonEon() {
   echo "Installing ArgonEon..."
   curl https://download.argon40.com/argoneon.sh | bash  || error "Failed to install ArgonEon."
   echo "ArgonEon installed successfully."
 }
 
+
+# Install OpenMediaVault
 function install_openmediavault() {
   echo "Installing OpenMediaVault..."
-  sudo wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash   || error "Failed to install OpenMediaVault."
-  echo "OpenMediaVault installed successfully."
+  sudo wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
+  
+  # Check if installation failed due to unsupported version
+  check_log_for_error
+  
 }
 
 
